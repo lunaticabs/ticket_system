3bde1b317e13899ad1a91eeec674292bdd4daac5
luna <lunaticabstraction@gmail.com>
2024-12-06 12:43:05 +0800

    initial commit.

---
+++ lib/data.ml
@@ -0,0 +1,31 @@
+(* Define vector (dynamic array) *)
+type 'a vector = {
+  compacity: int;
+  mutable index: int;
+  buffer: 'a array;
+}
+
+(* create a new vector *)
+let create com init= {
+  compacity = com;
+  index = 0;
+  buffer = Array.make com init;
+}
+
+(* function to enlarge the vector to 2 times larger *)
+let enlarge vec = {
+  compacity = 2 * vec.compacity;
+  index = vec.index;
+  buffer =
+    let new_vec = Array.make (2 * vec.compacity) vec.buffer.(0) in
+    Array.blit vec.buffer 0 new_vec 0 vec.compacity;
+    new_vec
+}
+
+
+let rec push x vec =
+  if vec.index == vec.compacity then
+    push x (enlarge vec)
+  else
+    vec.buffer.(vec.index) <- x;
+    vec.index <- vec.index + 1;
---
+++ lib/train.ml
@@ -0,0 +1,39 @@
+(* open Core.Array *)
+
+(* 定义车次信息的记录类型 *)
+type station_info = {
+  station_name: string;
+  arrival_time: string;
+  departure_time: string;
+  distance: float;
+}
+
+type train_info = {
+  train_id: string;
+  start_station: string;
+  end_station: string;
+  departure_time: string;
+  ticket_price: float;
+  is_operating: bool;
+  route: station_info list; (* 停靠站信息 *)
+}
+
+(* 定义高铁车次信息的存储 *)
+type train_system = train_info list
+
+let add_train (system: train_system) (train: train_info) : train_system =
+  train :: system
+
+let stop_train (system: train_system) (train_id: string) : train_system =
+  List.map (fun t -> if t.train_id == train_id then { t with is_operating = false } else t) system
+
+(* let find_train_by_id (system: train_system) (train_id: string) : train_info option =
+  find_opt (fun t -> t.train_id = train_id) system
+
+let find_train_by_start_end (system: train_system) (start: string) (end_: string) : train_info list =
+  List.filter (fun t -> t.start_station = start && t.end_station = end_) system
+
+let find_train_by_time (system: train_system) (time: string) : train_info list =
+  List.filter (fun t -> t.departure_time = time) system *)
+
+
